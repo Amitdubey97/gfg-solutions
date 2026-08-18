90eb05c1d70df3a87d71f766c8d1174bfc521065
Amitdubey97 <133532387+Amitdubey97@users.noreply.github.com>
2026-08-18 19:17:25 +0530

    Update longest-consecutive-subsequence.cpp

--- Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.cpp
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.cpp
@@ -11,10 +11,10 @@ class Solution {
         st.insert(arr[i]);
         }
         for(auto it: st){
-            if(st.find(it -1) == st.end()){
+            if(st.find(it -1) == st.end()){// ye check karta hai ki it -1 (usse ek chhota no.)set me hai ya nahi, agar nahin iska matlab it kisi sequence ka starting pont hai.
                 int cnt = 1;
-                int x= it;
-                while(st.find(x+1)!= st.end()){
+                int x= it; // pehla element sequence ka
+                while(st.find(x+1)!= st.end()){// jab tak agla consecutive no.(x+1)  milta rahe count ko badhate raho.
                     x = x+1;
                     cnt = cnt +1;
                 }
